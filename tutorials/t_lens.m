807fbf2ce945ce21ee44ca961fb3d9f5da6c85f9
Brian Wandell <wandell@stanford.edu>
2018-03-05 20:30:23 -0800

    Fixed lens to lensC

--- tutorials/t_lens.m
+++ tutorials/t_lens.m
@@ -5,12 +5,12 @@
 % See also
 
 %% Find the lenses
-thisLens = lens;
+thisLens = lensC;
 lenses = thisLens.list;
 
 %% Pick a lens and draw it and plot the focal distance
 
-thisLens = lens('filename',lenses(5).name);
+thisLens = lensC('filename',lenses(5).name);
 thisLens.draw;
 
 %% Draw the lens focal length as a function of object distance
