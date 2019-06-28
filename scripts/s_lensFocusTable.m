145b24faff1d60bf0d7962e142029a1e6ed2dfc4
Brian Wandell <wandell@stanford.edu>
2019-06-27 17:47:27 -0700

    Cleaned up lens focus table creation and made the new FL files

--- scripts/s_lensFocusTable.m
+++ scripts/s_lensFocusTable.m
@@ -34,7 +34,7 @@
 lensDir = fullfile(ilensRootPath,'data','lens');
 
 % wide, tessar, fisheye, dgauss, telephoto, 2el, 2EL
-lensFiles = dir(fullfile(lensDir,'*.dat'));   
+lensFiles = dir(fullfile(lensDir,'*.json'));   
 
 % Range of distances in millimeters
 % From about 1 mm to 10 m.  There are thirty sample distances
@@ -43,29 +43,25 @@ objDistance = logspace(0.1,4,30);
 %% Calculate the focal distances
 
 nFiles = numel(lensFiles);
-allFilmDistance = zeros(nFiles,length(objDistance));
+fprintf('Processing %d lens files\n',nFiles);
+allFilmDistance  = zeros(nFiles,length(objDistance));
 allFocalLengths  = zeros(nFiles,1);
 
-for ii=1:length(lensFiles)
+for ii=1:nFiles
     fname = fullfile(lensDir,lensFiles(ii).name);
     for jj=1:length(objDistance)
         allFilmDistance(ii,jj) = lensFocus(lensFiles(ii).name,objDistance(jj));
     end
     % We should probably store this, too.
     allFocalLengths(ii) = lensFocus(lensFiles(ii).name,max(objDistance)*10^6);
-
 end
 
-% All the negative values could not be focused.  We set to Nan
-allFilmDistance(allFilmDistance < 0) = NaN;
 
 %%  When the distance is too small, we can't get a good focus.
-
-% This plots the object and film distances for all of the lenses
-vcNewGraphWin;
-loglog(objDistance,allFilmDistance');
-xlabel('Object distance (mm)'); ylabel('Film distance (mm)');
-grid on
+%
+%   The negative values could not be focused, so we set to NaN
+%
+allFilmDistance(allFilmDistance < 0) = NaN;
 
 %%  Write out the film distance as a function of object distance
 %
@@ -76,23 +72,36 @@ grid on
 % and the lens focal length, which would be derived by using a very
 % far distance
 
+hdl = ieNewGraphWin; 
+xlabel('Object distance (mm)'); ylabel('Film distance (mm)');
+grid on; hold on;
+
 saveObjDistance   = objDistance;
-for ii=1:length(lensFiles)
+
+for ii=1:nFiles
     
-    filmDistance = allFilmDistances(ii,:);   % This lens distance
+    filmDistance = allFilmDistance(ii,:);   % This lens distance
     
-    idx = find(isnan(filmDistance),1,'last'); % First NaN
-    closestDistance = saveObjDistance(idx);
+    % If there is a NaN, find the index right after it.  Otherwise,
+    % the returned index is 1.
+    idx = find(isnan(filmDistance),1,'last'); 
+    if idx < length(filmDistance), idx = idx+1;
+    else, idx = 1;
+    end
     
-    idx = find(~isnan(filmDistance),1,'first'); % Remove NaNs
+    closestDistance = saveObjDistance(idx);    
+    
     filmDistance = filmDistance(idx:end);
-    objDistance = saveObjDistance(idx:end);
+    objDistance  = saveObjDistance(idx:end);
+    focalLength  = allFocalLengths(ii);
     
-    focalLength = allFocalLengths(ii);
-    
+    % This plots the object and film distances for all of the lenses
+    loglog(objDistance,filmDistance);
+
     [p,n,~] = fileparts(lensFiles(ii).name);
     flFile = fullfile(lensDir,[n,'.FL.mat']);
     save(flFile,'objDistance','filmDistance','focalLength','closestDistance');
 end
+set(gca,'xscale','log','yscale','log');
 
 %%
