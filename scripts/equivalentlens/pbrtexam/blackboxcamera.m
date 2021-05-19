c3c4e395fd24b7ed6339b390a769a78487838c72
ZhengLyu <zhenglyu@stanford.edu>
2021-05-19 15:07:49 -0700

    Added some comments

--- scripts/equivalentlens/pbrtexam/blackboxcamera.m
+++ scripts/equivalentlens/pbrtexam/blackboxcamera.m
@@ -1,8 +1,10 @@
-%% Normal rendering from ISET3d 
+%% PART1: Normal rendering from ISET3d 
+
 ieInit;
 if ~piDockerExists, piDockerConfig; end
 
-%
+% This section has to be run at least once in order to use the second part
+% of code below
 thisR = piRecipeDefault('scene name', 'simple scene');
 lensfile = 'lenses/dgauss.22deg.3.0mm.json';
 
@@ -12,13 +14,13 @@ thisR.set('aperture diameter', 2.5318);
 thisR.set('film distance', 0.00216675);
 thisR.set('film diagonal', 5); % mm
 
-piWrite(thisR);
 
-%
+% OPTIONAL: Only useful for actual normal PBRTrendering
+piWrite(thisR);
 [oi, result] = piRender(thisR, 'render type', 'radiance');
 oiWindow(oi);
 
-%% Compare results after running seperately from PBRT
+%% PART2: Compare results after running seperately from PBRT
 % This part needs to be run not within ISET3d but directly from PBRT. Later
 % on we would make blackbox as a docker container that can be also called
 % from ISET3d.
