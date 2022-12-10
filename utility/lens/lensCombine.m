ef6841ba547bdc47c2380a5be740df39fc6ddf49
Brian Wandell <wandell@stanford.edu>
2022-12-09 19:20:57 -0800

    Update lensCombine.m

--- utility/lens/lensCombine.m
+++ utility/lens/lensCombine.m
@@ -1,23 +1,38 @@
 function [combinedLens,uLens, iLens, dockerCmd] = lensCombine(uLens,iLens,uLensHeight,nMicrolens)
-% Combines the microlens and imaging lens into a single lens file
+% Deprecated:  Combines the microlens and imaging lens into a single
+% lens file.  We are now performing this using piMicrolensWrite.
 %
 % Synopsis
 %  [combinedLens,filmHeight,filmWidth,dockerCmd] = lensCombine(uLens,iLens,uLensHeight,nMicrolens)
 %
 % Brief description
+%  Create a json file that incorporates the imaging lens and the
+%  microlens information in a format that can be used by PBRT (omni
+%  camera).  This method is used for calculating the impact of
+%  microlenses.
 %   
 % Inputs:
+%   uLens - Microlens file name
+%   iLens - Imaging lens file name
+%   uLensHeight - Not sure what this is doing here
+%   nMicrolens  - 2-vector of microlens counts in x,y
 %
 % Optional key/value pairs
+%    N/A
 %
 % Outputs:
+%   combinedLens = file name of the combined lens
+%   uLens - ISETLens lensC
+%   iLens - ISETLens lensC
+%   dockerCmd - Command used to call the lenstool
 %
 % Description:
-%  Choose an even number for nMicrolens.  
-%  This assures that the sensor and ip data have the right integer relationships. 
+%  
+%  Choose an even number for nMicrolens.  This assures that the sensor
+%  and ip data have the right integer relationships.  
 %
 % See also
-%
+%   piMicrolensWrite
 
 %% Parse
 
